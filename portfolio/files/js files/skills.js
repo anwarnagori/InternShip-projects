dda1aef4a6ab35dbf3b8009408d6ef4ec09f1455
anwarnagori <ar1498137@gmail.com>
2024-08-22 14:46:48 -0700

    about & skills page incomplete

---
+++ portfolio/files/js files/skills.js
@@ -0,0 +1,44 @@
+const options = document.querySelectorAll("label");
+
+function skillsPoll() {
+    for (let i = 0; i < options.length; i++) {
+        options[i].addEventListener("click", () => {
+            for (let j = 0; j < options.length; j++) {
+                if (options[j].classList.contains("selected"));
+                else{
+                    options[j].classList.remove("selected");
+                }
+            }
+        })
+        options[i].classList.add("selected");
+        for (let k = 0; k < options.length; k++) {
+            options[k].classList.add("selected");
+        }
+        let forVail = options[i].getAttribute("for");
+        let selectInput = document.querySelector("#" + forVail);
+        let getAtt = selectInput.getAttribute("type");
+
+        if (getAtt == "checkbox") {
+            selectInput.getAttribute("type", "radio");
+        }
+        else if (selectInput.checked == true) {
+            options[i].classList.remove("selected");
+            selectInput.setAttribute("type", "checkbox");
+        }
+
+        let array = [];
+
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].classList.contains("selected")) {
+                array.push(1);
+            }
+        }
+        if (array.length == 0) {
+            for (let m = 0; m < options.length; m++) {
+                options[m].removeAttribute("class");
+            }
+        }
+    }
+}
+
+skillsPoll();
