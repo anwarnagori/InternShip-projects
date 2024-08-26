3a47b99d11ebe9c9f0198b2d4e6ee0b395908c90
anwarnagori <ar1498137@gmail.com>
2024-08-26 05:48:46 -0700

    incomplete

---
+++ portfolio/files/js files/contect.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const themeToggle = document.getElementById('img');
+
+//     themeToggle.addEventListener('click', () => {
+//         // Toggle background color
+//         if (document.body.style.backgroundColor === 'red') {
+//             document.body.style.backgroundColor = '#000000'; // Original color
+//         } else {
+//             document.body.style.backgroundColor = 'red'; // New color
+//         }
+//     });
+// });
